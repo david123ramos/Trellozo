2ba52a70f68ee742e22b52019dc138ba7632beb9
David <david-ramos123@hotmail.com>
2019-10-28 17:32:18 -0300

    melhorias no layout

--- src/main.js
+++ src/main.js
@@ -66,7 +66,7 @@ class Board {
         boardF.setAttribute("class", "col-12 col-sm-3 col-md-3 col-lg-2 col-xl-2 ");
         
         let div1 = document.createElement("div");
-        div1.setAttribute("class", "card border-light");
+        div1.setAttribute("class", "card border-light h-100 mt-4.5");
         div1.setAttribute("id", "newBoard");
         div1.style.backgroundColor = this.color;
         //TODO: o onclick desses boards deve abrir as listas do trello
