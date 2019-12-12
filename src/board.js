05e525c17d3923ef888a18db6f9e79827af2613f
David <drs@a.recife.ifpe.edu.br>
2019-12-12 20:24:09 -0300

    Refatorando javascript

--- src/board.js
+++ src/board.js
@@ -4,7 +4,8 @@ const boardColor = JSON.parse(sessionStorage.getItem("board")).color;
 const boardName = JSON.parse(sessionStorage.getItem("board")).name;
 const token = sessionStorage.getItem("token");
 const data = dataAtualFormatada();
-
+var title = document.getElementById("cardModal").getElementsByTagName("textarea")[0];
+var activeKeydown = false;
 //flag que indica se a data do card foi alterada
 var dateChange = false;
 
@@ -53,267 +54,6 @@ window.onload = function () {
             addTag(this);
         });
     });
-}
-
-
-/*Classe que define uma lista */
-class List {
-    constructor(name, listId) {
-        this.name = name;
-        this.listId = listId;
-    }
-
-    //a lista é uma outra lista(de cards) dentro dentro da "lista Pai"
-    init() {
-        //Hierarquia: li->div->ul->li->div->span->->textNode
-        let li = document.createElement("li");
-        li.setAttribute("class", "col-12 col-sm-3 col-md-3 col-lg-2 col-xl-2 mb-3 p-0 mr-2 ml-0");
-
-
-
-        var ul = document.createElement("ul");
-        ul.setAttribute("class", "p-1 list defaultBgcolor");
-        ul.setAttribute("id", this.listId)
-
-        ul.ondrop = function () { drop(event) };
-
-        ul.ondragover = function (event) { allowDrop(event); this.classList.add("dragOn") };
-
-        ul.ondragleave = function () { this.classList.remove("dragOn"); }
-
-        ul.ondragend = function () {
-            this.classList.remove("dragOn");
-            document.getElementsByClassName("spnDelete-ev")[0].classList.replace("spnDelete-ev", "spnDelete")
-        }
-
-        let divMain = document.createElement("div");
-        divMain.setAttribute("class", " defaultBgcolor rounded pb-1");
-
-
-        let firstChild = document.createElement("div");
-        firstChild.appendChild(new BtnIsertCard().init());
-
-        firstChild.addEventListener("click", function () {
-            let newLi = document.createElement("li");
-            newLi.setAttribute("draggable","false")
-
-            let close = new CloseButton().init();
-            let add = new AddButton().init();
-
-
-            //o botao de fechar precisa remover o item(form) que foi appendado na lista
-            close.addEventListener("click", () => {
-                ul.removeChild(newLi);
-                showAlert(firstChild);
-            });
-
-
-            //form -> textArea -> divWrapper -> botaoAdd -> botaoClose
-            let form = new formCard().init();
-            form.getElementsByTagName("textarea")[0].addEventListener("keypress", function (event) {
-                if (event.keyCode == 13 && this.value != "") {
-                    createCard(this.value, ul.id);
-                    form.reset();
-                    this.blur();
-                } else if (event.keyCode == 13) {
-                    event.preventDefault();
-                }
-            })
-
-            form.addEventListener("submit", function (e) {
-                e.preventDefault();
-                if (this.getElementsByTagName("textarea")[0].value != "") {
-                    createCard(this.getElementsByTagName("textarea")[0].value, ul.id);
-                    this.reset();
-                    this.getElementsByTagName("textarea")[0].focus();
-                }
-            });
-
-
-            let divWrapper = new DivWrapper().init();
-            divWrapper.appendChild(add);
-            divWrapper.appendChild(close);
-            form.appendChild(divWrapper);
-            newLi.appendChild(form);
-            ul.appendChild(newLi);
-            closeAlert(firstChild);
-
-        });
-
-
-        let div = document.createElement("div");
-        div.setAttribute("class", "title defaultBgcolor d-flex justify-content-between");
-
-        //nome da lista
-        let text = new NameList(this.name, this.listId).init();
-
-        //icone "..." que aparece quando se coloca o mouse em cima da lista
-        let buttonDelete = document.createElement("button");
-        let iconDots = document.createElement("img");
-        buttonDelete.setAttribute("class", "more-after btn p-0");
-        buttonDelete.setAttribute("data-toggle", "modal");
-        buttonDelete.setAttribute("data-target", "#modalExcluirLista");
-        buttonDelete.onclick = () => {
-            sessionStorage.setItem("list", this.listId);
-        }
-
-        iconDots.setAttribute("src", "img/deleteList.png");
-        buttonDelete.appendChild(iconDots);
-        divMain.onmouseover = function () {
-            buttonDelete.classList.remove("more-after");
-        }
-        divMain.onmouseleave = function () {
-            buttonDelete.classList.add("more-after");
-        }
-
-        div.appendChild(text);
-        div.appendChild(buttonDelete);
-        divMain.appendChild(div);
-        divMain.appendChild(firstChild);
-        divMain.insertBefore(ul, firstChild);
-
-        li.appendChild(divMain);
-        return li;
-    }
-}
-
-/*Classe que define o input que recebe o nome da lista */
-class NameList {
-    constructor(listName, listId) {
-        this.listName = listName;
-        this.listId = listId;
-    }
-    init() {
-        let text = document.createElement("input");
-        text.setAttribute("maxlength", 15);
-        text.setAttribute("autocomplete", "off");
-        text.setAttribute("autocorrect", "off");
-        text.setAttribute("class", "font-weight-bold mt-1 mr-1 list-name success mw-50");
-        text.value = this.listName;
-
-        text.addEventListener("keydown", (e) => {
-            renameList(e, text, this.listId);
-        });
-        return text;
-    }
-}
-
-/*Botão usado para inserir um novo card dentro de uma lista*/
-class BtnIsertCard {
-    init() {
-        let div = document.createElement("a");
-        div.setAttribute("class", "d-flex justify-content-center mw-100");
-        let text = document.createTextNode("+ Add another card");
-        div.classList.add("secondaryText");
-        div.appendChild(text);
-        return div;
-    }
-}
-
-/*Classe que define um card */
-class Card {
-    constructor(cardName, cardId, date) {
-        this.cardName = cardName;
-        this.cardId = cardId;
-        this.date = date;
-    }
-
-    init() {
-        let div = document.createElement("div");
-        div.setAttribute("class", "card p-2 textCardSubmited divTextArea");
-
-        let ulTag = document.createElement("ul");
-        ulTag.setAttribute("class","list-inline");
-        div.appendChild(ulTag);
-        
-        let spnNome = document.createElement("span");
-        spnNome.innerText = this.cardName;
-        div.appendChild(spnNome);
-        let liCard = document.createElement("li");
-        liCard.setAttribute("id", this.cardId);
-
-        liCard.setAttribute("data-toggle", "modal");
-        liCard.setAttribute("data-target", "#cardModal");
-
-        liCard.onclick = () => {
-            var name = document.getElementById(this.cardId).firstElementChild.querySelector("span").innerText;
-            let cardInfo = {
-                "name": name,
-                "id": this.cardId,
-                "date": this.date
-            }
-            sessionStorage.setItem("card", JSON.stringify(cardInfo));
-            //limpa o modal do card antes de ele ser aberto
-            clearModalCard();
-            openCard();
-        }
-        
-        liCard.setAttribute("draggable", true);
-        liCard.ondragstart = function () { this.classList.add("drag"); div.classList.add("noShadow"); drag(event) };
-        liCard.ondragend = function () { this.classList.remove("drag"); div.classList.remove("noShadow") }
-        let date = document.createElement("div");
-        date.setAttribute("date", this.date);
-
-        div.appendChild(date);
-        liCard.appendChild(div);
-        return liCard;
-    }
-}
-
-/*Classe que define o form para a criação de um card */
-class formCard {
-
-    init() {
-        let form = document.createElement("form");
-        let div = document.createElement("div");
-        div.setAttribute("class", "divTextArea ");
-
-        let textArea = document.createElement("textarea");
-        textArea.setAttribute("placeholder", "Enter a title for this card...")
-        textArea.classList.add("textCard")
-        div.appendChild(textArea);
-        form.appendChild(div);
-
-        return form;
-    }
-}
-
-
-class DivWrapper {
-    init() {
-        let div = document.createElement('div');
-        div.setAttribute("class", "d-flex justify-content-start p-1");
-        return div;
-    }
-}
-
-class AddButton {
-    init() {
-
-        let button = document.createElement("button");
-        button.setAttribute("class", "btn btn-success btn-textArea");
-        button.setAttribute("type", "submit");
-        button.appendChild(document.createTextNode("Add card"));
-
-        return button;
-    }
-}
-
-class CloseButton {
-    init() {
-
-        let close = document.createElement("button");
-        close.setAttribute("type", "button");
-        close.classList.add("close");
-
-
-        let spn = document.createElement("span");
-        spn.appendChild(document.createTextNode("x"));
-        close.appendChild(spn);
-
-
-        return close;
-    }
 }
 
 formCreateList.addEventListener("submit", function (e) {
@@ -407,6 +147,7 @@ function closeAlert(someElement) {
         someElement.classList.add("none");
     }
 }
+
 function showAlert(someElement) {
     someElement.classList.remove("none");
     someElement.classList.add("block");
@@ -415,6 +156,7 @@ function showAlert(someElement) {
 function mainPage() {
     return window.location.href = "main.html";
 }
+
 function logout(){
     sessionStorage.clear();
     return window.location.href = "index.html";
@@ -785,8 +527,6 @@ function clearModalCard(){
 
 }
 
-var title = document.getElementById("cardModal").getElementsByTagName("textarea")[0];
-var activeKeydown = false;
 //Função que é chamada quando o card é clicado
 function openCard() {
 
@@ -846,7 +586,6 @@ function appendTag(id, colorTag){
     tagList.insertBefore(li, document.getElementById("firstChildTag"));
 }
 
-
 function addComment(textarea, comment, card_id){
     
     appendComment(comment);   
@@ -874,7 +613,6 @@ function addComment(textarea, comment, card_id){
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send(JSON.stringify(comment4Send));
 }
-
 
 //recupera os comentários de um card
 function cardComments (card_id) {
@@ -983,7 +721,6 @@ function deleteCardFromModal(){
     xhttp.send(JSON.stringify(card));
 }
 
-
 //função de deleção do card a partir do drag and drop
 function deleteCard(e) {
     e.preventDefault();
@@ -1066,7 +803,6 @@ function changeDate(date, card_id){
     xhttp.send(JSON.stringify(newDate));
 }
 
-
 //função usada para mudar o card de lista
 function changeCard(card_id, list_id) {
     let change = {
@@ -1090,7 +826,6 @@ function changeCard(card_id, list_id) {
     xhttp.send(JSON.stringify(change));
 }
 
-
 //retorna  a cor normalizada bootstrap dada o id da tag
 function getColorFromId(button_id){
     var colorTag;
@@ -1106,7 +841,6 @@ function getColorFromId(button_id){
     }
     return colorTag;
 }
-
 
 //adiciona as tags nos cards
 function addTagOnCard(card_id, color){
---
+++ src/classes.js
@@ -0,0 +1,260 @@
+
+/*Classe que define uma lista */
+class List {
+    constructor(name, listId) {
+        this.name = name;
+        this.listId = listId;
+    }
+
+    //a lista é uma outra lista(de cards) dentro dentro da "lista Pai"
+    init() {
+        //Hierarquia: li->div->ul->li->div->span->->textNode
+        let li = document.createElement("li");
+        li.setAttribute("class", "col-12 col-sm-3 col-md-3 col-lg-2 col-xl-2 mb-3 p-0 mr-2 ml-0");
+
+
+
+        var ul = document.createElement("ul");
+        ul.setAttribute("class", "p-1 list defaultBgcolor");
+        ul.setAttribute("id", this.listId)
+
+        ul.ondrop = function () { drop(event) };
+
+        ul.ondragover = function (event) { allowDrop(event); this.classList.add("dragOn") };
+
+        ul.ondragleave = function () { this.classList.remove("dragOn"); }
+
+        ul.ondragend = function () {
+            this.classList.remove("dragOn");
+            document.getElementsByClassName("spnDelete-ev")[0].classList.replace("spnDelete-ev", "spnDelete")
+        }
+
+        let divMain = document.createElement("div");
+        divMain.setAttribute("class", " defaultBgcolor rounded pb-1");
+
+
+        let firstChild = document.createElement("div");
+        firstChild.appendChild(new BtnIsertCard().init());
+
+        firstChild.addEventListener("click", function () {
+            let newLi = document.createElement("li");
+            newLi.setAttribute("draggable","false")
+
+            let close = new CloseButton().init();
+            let add = new AddButton().init();
+
+
+            //o botao de fechar precisa remover o item(form) que foi appendado na lista
+            close.addEventListener("click", () => {
+                ul.removeChild(newLi);
+                showAlert(firstChild);
+            });
+
+
+            //form -> textArea -> divWrapper -> botaoAdd -> botaoClose
+            let form = new formCard().init();
+            form.getElementsByTagName("textarea")[0].addEventListener("keypress", function (event) {
+                if (event.keyCode == 13 && this.value != "") {
+                    createCard(this.value, ul.id);
+                    form.reset();
+                    this.blur();
+                } else if (event.keyCode == 13) {
+                    event.preventDefault();
+                }
+            })
+
+            form.addEventListener("submit", function (e) {
+                e.preventDefault();
+                if (this.getElementsByTagName("textarea")[0].value != "") {
+                    createCard(this.getElementsByTagName("textarea")[0].value, ul.id);
+                    this.reset();
+                    this.getElementsByTagName("textarea")[0].focus();
+                }
+            });
+
+
+            let divWrapper = new DivWrapper().init();
+            divWrapper.appendChild(add);
+            divWrapper.appendChild(close);
+            form.appendChild(divWrapper);
+            newLi.appendChild(form);
+            ul.appendChild(newLi);
+            closeAlert(firstChild);
+
+        });
+
+
+        let div = document.createElement("div");
+        div.setAttribute("class", "title defaultBgcolor d-flex justify-content-between");
+
+        //nome da lista
+        let text = new NameList(this.name, this.listId).init();
+
+        //icone "..." que aparece quando se coloca o mouse em cima da lista
+        let buttonDelete = document.createElement("button");
+        let iconDots = document.createElement("img");
+        buttonDelete.setAttribute("class", "more-after btn p-0");
+        buttonDelete.setAttribute("data-toggle", "modal");
+        buttonDelete.setAttribute("data-target", "#modalExcluirLista");
+        buttonDelete.onclick = () => {
+            sessionStorage.setItem("list", this.listId);
+        }
+
+        iconDots.setAttribute("src", "img/deleteList.png");
+        buttonDelete.appendChild(iconDots);
+        divMain.onmouseover = function () {
+            buttonDelete.classList.remove("more-after");
+        }
+        divMain.onmouseleave = function () {
+            buttonDelete.classList.add("more-after");
+        }
+
+        div.appendChild(text);
+        div.appendChild(buttonDelete);
+        divMain.appendChild(div);
+        divMain.appendChild(firstChild);
+        divMain.insertBefore(ul, firstChild);
+
+        li.appendChild(divMain);
+        return li;
+    }
+}
+
+/*Classe que define o input que recebe o nome da lista */
+class NameList {
+    constructor(listName, listId) {
+        this.listName = listName;
+        this.listId = listId;
+    }
+    init() {
+        let text = document.createElement("input");
+        text.setAttribute("maxlength", 15);
+        text.setAttribute("autocomplete", "off");
+        text.setAttribute("autocorrect", "off");
+        text.setAttribute("class", "font-weight-bold mt-1 mr-1 list-name success mw-50");
+        text.value = this.listName;
+
+        text.addEventListener("keydown", (e) => {
+            renameList(e, text, this.listId);
+        });
+        return text;
+    }
+}
+
+/*Botão usado para inserir um novo card dentro de uma lista*/
+class BtnIsertCard {
+    init() {
+        let div = document.createElement("a");
+        div.setAttribute("class", "d-flex justify-content-center mw-100");
+        let text = document.createTextNode("+ Add another card");
+        div.classList.add("secondaryText");
+        div.appendChild(text);
+        return div;
+    }
+}
+
+/*Classe que define um card */
+class Card {
+    constructor(cardName, cardId, date) {
+        this.cardName = cardName;
+        this.cardId = cardId;
+        this.date = date;
+    }
+
+    init() {
+        let div = document.createElement("div");
+        div.setAttribute("class", "card p-2 textCardSubmited divTextArea");
+
+        let ulTag = document.createElement("ul");
+        ulTag.setAttribute("class","list-inline");
+        div.appendChild(ulTag);
+        
+        let spnNome = document.createElement("span");
+        spnNome.innerText = this.cardName;
+        div.appendChild(spnNome);
+        let liCard = document.createElement("li");
+        liCard.setAttribute("id", this.cardId);
+
+        liCard.setAttribute("data-toggle", "modal");
+        liCard.setAttribute("data-target", "#cardModal");
+
+        liCard.onclick = () => {
+            var name = document.getElementById(this.cardId).firstElementChild.querySelector("span").innerText;
+            let cardInfo = {
+                "name": name,
+                "id": this.cardId,
+                "date": this.date
+            }
+            sessionStorage.setItem("card", JSON.stringify(cardInfo));
+            //limpa o modal do card antes de ele ser aberto
+            clearModalCard();
+            openCard();
+        }
+        
+        liCard.setAttribute("draggable", true);
+        liCard.ondragstart = function () { this.classList.add("drag"); div.classList.add("noShadow"); drag(event) };
+        liCard.ondragend = function () { this.classList.remove("drag"); div.classList.remove("noShadow") }
+        let date = document.createElement("div");
+        date.setAttribute("date", this.date);
+
+        div.appendChild(date);
+        liCard.appendChild(div);
+        return liCard;
+    }
+}
+
+/*Classe que define o form para a criação de um card */
+class formCard {
+
+    init() {
+        let form = document.createElement("form");
+        let div = document.createElement("div");
+        div.setAttribute("class", "divTextArea ");
+
+        let textArea = document.createElement("textarea");
+        textArea.setAttribute("placeholder", "Enter a title for this card...")
+        textArea.classList.add("textCard")
+        div.appendChild(textArea);
+        form.appendChild(div);
+
+        return form;
+    }
+}
+
+
+class DivWrapper {
+    init() {
+        let div = document.createElement('div');
+        div.setAttribute("class", "d-flex justify-content-start p-1");
+        return div;
+    }
+}
+
+class AddButton {
+    init() {
+
+        let button = document.createElement("button");
+        button.setAttribute("class", "btn btn-success btn-textArea");
+        button.setAttribute("type", "submit");
+        button.appendChild(document.createTextNode("Add card"));
+
+        return button;
+    }
+}
+
+class CloseButton {
+    init() {
+
+        let close = document.createElement("button");
+        close.setAttribute("type", "button");
+        close.classList.add("close");
+
+
+        let spn = document.createElement("span");
+        spn.appendChild(document.createTextNode("x"));
+        close.appendChild(spn);
+
+
+        return close;
+    }
+}
